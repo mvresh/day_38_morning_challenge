5816291b466b991201a6ce81de8d246dbe27cc82
Varun Resh Maddali <mvresh@gmail.com>
2019-11-27 16:01:51 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -12,5 +12,29 @@
 //    [3,2,1]
 //  ]
 
+List<List<int>> permutations(List<int> numbers){
+  List<List<int>> permutationsList = [];
+  void doPermutations(List<int> list, int current) {
+
+    if (current == list.length) {
+      //terminating case
+      print(list);
+      permutationsList.add(list);
+      return;
+    }
+
+    for (int i = current; i < list.length; i++) {
+      List<int> permutation = List.from(list);
+      permutation[current] = list[i];
+      permutation[i] = list[current];
+      doPermutations(permutation, current + 1);
+    }
+  }
+  doPermutations(numbers, 0);
+  return permutationsList;
+}
+
+
 main() {
+  print(permutations([1,2,3,4,5,6]));
 }
